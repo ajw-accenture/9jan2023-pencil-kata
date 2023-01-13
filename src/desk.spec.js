74ba5d1cc4cd80d9d38218f7e483fe0fe9ff975d
ajw-accenture <ajw-accenture@users.noreply.github.com>
2023-01-13 11:34:50 -0600

    refactor: pull up the writing data object

--- src/desk.spec.js
+++ src/desk.spec.js
@@ -1,14 +1,21 @@
 const {write} = require('./desk');
 
 describe('Writing', () => {
+  let writingData = {};
+
+  beforeEach(() => {
+    writingData = { paper: '' };
+  });
+
   it('should write on a piece of paper', () => {
-    const {paper} = write('Hello, world!', {});
+    const {paper} = write('Hello, world!', writingData);
     
     expect(paper).toBe('Hello, world!');
   });
 
   it('should write after any text already written on the piece of paper', () => {
-    const {paper} = write('a little lamb', {paper: 'Mary had '});
+    const data = { ...writingData, paper: 'Mary had '};
+    const {paper} = write('a little lamb', data);
 
     expect(paper).toBe('Mary had a little lamb');
   });
