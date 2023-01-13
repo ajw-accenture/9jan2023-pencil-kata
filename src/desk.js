8aafa2dddc7a7f8877c3a222a382c3a999fa9695
ajw-accenture <ajw-accenture@users.noreply.github.com>
2023-01-13 11:34:51 -0600

    misc: extract magic, add IDEA and VSCode to ignore

--- src/desk.js
+++ src/desk.js
@@ -1,29 +1,31 @@
 const MAX_SHARPNESS = 1000;
+const SINGLE_SPACE = ' ';
+const EMPTY = '';
 
 const write = (pencil, toWrite) => {
   const { paper, sharpness } = pencil;
 
   return toWrite
-    .split('')
+    .split(EMPTY)
     .reduce((accumulator, character) => {
       let sharpnessReduction = character === character.toUpperCase() ? 2 : 1;
       let charToWrite = character;
 
-      if (character === ' ' || character === '\n') {
+      if (character === SINGLE_SPACE || character === '\n') {
         sharpnessReduction = 0;
       }
 
       const currentSharpness = accumulator.sharpness;
       if (currentSharpness <= 0) {
         sharpnessReduction = 0;
-        charToWrite = ' ';
+        charToWrite = SINGLE_SPACE;
       }
 
       return {
         paper: `${accumulator.paper}${charToWrite}`,
         sharpness: currentSharpness - sharpnessReduction
       };
-    }, { paper: paper || '', sharpness });
+    }, { paper: paper || EMPTY, sharpness });
 };
 
 const sharpen = data => ({ ...data, sharpness: MAX_SHARPNESS });
