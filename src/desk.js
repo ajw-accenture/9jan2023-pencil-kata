177fdd155af831290d4d51e45468bad8c148564b
ajw-accenture <ajw-accenture@users.noreply.github.com>
2023-01-13 11:34:51 -0600

    refactor: eslint clean up

--- src/desk.js
+++ src/desk.js
@@ -1,34 +1,34 @@
 const MAX_DURABILITY = 1000;
 
 const write = (thing, data) => {
-    const {paper, durability} = data;
+  const {paper, durability} = data;
 
-    return thing
-        .split('')
-        .reduce((accumulator, character) => {
-            let duraReduction = character === character.toUpperCase() ? 2 : 1;
-            let charToWrite = character;
+  return thing
+    .split('')
+    .reduce((accumulator, character) => {
+      let duraReduction = character === character.toUpperCase() ? 2 : 1;
+      let charToWrite = character;
 
-            if (character === ' ' || character === '\n') {
-                duraReduction = 0;
-            }
+      if (character === ' ' || character === '\n') {
+        duraReduction = 0;
+      }
 
-            const currentDura = accumulator.durability;
-            if (currentDura <= 0) {
-                duraReduction = 0;
-                charToWrite = ' ';
-            }
+      const currentDura = accumulator.durability;
+      if (currentDura <= 0) {
+        duraReduction = 0;
+        charToWrite = ' ';
+      }
 
-            return {
-                paper: `${accumulator.paper}${charToWrite}`,
-                durability: currentDura - duraReduction
-            };
-        }, {paper: paper || '', durability});
+      return {
+        paper: `${accumulator.paper}${charToWrite}`,
+        durability: currentDura - duraReduction
+      };
+    }, {paper: paper || '', durability});
 };
 
 const sharpen = data => ({...data, durability: MAX_DURABILITY});
 
 module.exports = {
-    write,
-    sharpen
+  write,
+  sharpen
 };
