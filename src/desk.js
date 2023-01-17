70708bc986a0498716b004c1eb0fcd09125ac67d
ajw-accenture <ajw-accenture@users.noreply.github.com>
2023-01-17 08:31:49 -0600

    refactor: many things...
    * remove extra property on _erase() return object
    * 'utensil' renamed to 'utensils'
    * pull out a few helpers

--- src/desk.js
+++ src/desk.js
@@ -4,26 +4,40 @@ const EMPTY = '';
 const AROBASE = '@';
 const NEWLINE = '\n';
 
-const _overwrite = (utensil, token, atIndex) => {
-  const { paper, pencil } = utensil;
+const _calculateNextSharpness = (currentSharpness, letter) => {
+  let reduceBy = 1;
+
+  if (currentSharpness === 0 || letter === SINGLE_SPACE || letter === NEWLINE) {
+    reduceBy = 0;
+  } else if (letter === letter.toUpperCase()) {
+    reduceBy = 2;
+  }
+
+  return currentSharpness - reduceBy;
+};
+
+const _findLastIndexOfToken = (utensils, token) => utensils.paper.lastIndexOf(token);
+
+const _overwrite = (utensils, token, atIndex) => {
+  const { paper, pencil } = utensils;
   const paperLetters = paper.split(EMPTY);
   const tokenLetters = token.split(EMPTY);
   const stopAt = atIndex + tokenLetters.length;
   let sharpness = pencil.sharpness;
 
   for (let i = atIndex, p = 0; i < stopAt; i++, p++) {
-    const currentCharOnPaper = paperLetters[i];
+    const letterOnPaper = paperLetters[i];
     const letterToWrite = tokenLetters[p];
 
-    if (currentCharOnPaper !== SINGLE_SPACE) {
+    if (letterOnPaper !== SINGLE_SPACE) {
       paperLetters[i] = AROBASE;
     } else {
       paperLetters[i] = letterToWrite;
-      sharpness -= (letterToWrite === letterToWrite.toUpperCase() ? 2 : 1);
+      sharpness = _calculateNextSharpness(sharpness, letterToWrite);
     }
   }
 
-  return { ...utensil, pencil: { ...pencil, sharpness }, paper: paperLetters.join(EMPTY) };
+  return { ...utensils, pencil: { ...pencil, sharpness }, paper: paperLetters.join(EMPTY) };
 };
 
 const _append = (utensils, token) => {
@@ -31,23 +45,17 @@ const _append = (utensils, token) => {
 
   return token
     .split(EMPTY)
-    .reduce((accumulator, character) => {
+    .reduce((accumulator, letter) => {
       const currentSharpness = accumulator.pencil.sharpness;
-      let charToWrite = character;
-      let sharpnessReduction = character === character.toUpperCase() ? 2 : 1;
-
-      if (character === SINGLE_SPACE || character === NEWLINE) {
-        sharpnessReduction = 0;
-      }
+      let letterToWrite = letter;
 
       if (currentSharpness <= 0) {
-        sharpnessReduction = 0;
-        charToWrite = SINGLE_SPACE;
+        letterToWrite = SINGLE_SPACE;
       }
 
       return {
-        paper: `${accumulator.paper}${charToWrite}`,
-        pencil: { sharpness: currentSharpness - sharpnessReduction }
+        paper: `${accumulator.paper}${letterToWrite}`,
+        pencil: { sharpness: _calculateNextSharpness(currentSharpness, letterToWrite) }
       };
     }, { paper: paper || EMPTY, pencil: { sharpness } });
 };
@@ -66,7 +74,7 @@ const sharpen = utensils => ({ ...utensils, pencil: { ...utensils.pencil, sharpn
 const _erase = (utensils, token) => {
   const { paper, pencil, pencil: { rubber } } = utensils;
   const tokenToErase = rubber < token.length ? token.substring(token.length - rubber) : token;
-  const indexOfToken = paper.lastIndexOf(tokenToErase);
+  const indexOfToken = _findLastIndexOfToken(utensils, tokenToErase);
 
   const upperHalf = paper.substring(0, indexOfToken);
   const lowerHalf = paper.substring(indexOfToken + tokenToErase.length);
@@ -75,7 +83,6 @@ const _erase = (utensils, token) => {
 
   return {
     ...utensils,
-    indexOfToken,
     pencil: { ...pencil, rubber: rubber - tokenToErase.length },
     paper: `${upperHalf}${spaces}${lowerHalf}`
   };
@@ -87,11 +94,11 @@ const erase = (utensils, toErase) => {
   return { ...utensils, pencil: { ...erased.pencil }, paper: erased.paper };
 };
 
-const edit = (utensils, atToken, editToken) => {
-  const erased = _erase(utensils, atToken);
-  const written = _overwrite(erased, editToken, erased.indexOfToken);
+const edit = (utensils, tokenToChange, tokenToWrite) => {
+  const indexOfErasedToken = _findLastIndexOfToken(utensils, tokenToChange);
+  const erased = _erase(utensils, tokenToChange);
 
-  return written;
+  return _overwrite(erased, tokenToWrite, indexOfErasedToken);
 };
 
 module.exports = { write, sharpen, erase, edit };
